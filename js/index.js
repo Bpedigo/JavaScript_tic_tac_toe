aab79ef51e026b707c8816d6d4127e0d9d631658
Brian Pedigo <bpedigo1@hotmail.com>
2025-03-30 08:34:21 -0700

    updated game

--- js/index.js
+++ js/index.js
@@ -1,409 +1,270 @@
-// variables
-var board = {
-  a1: "",
-  a2: "",
-  a3: "",
-  b1: "",
-  b2: "",
-  b3: "",
-  c1: "",
-  c2: "",
-  c3: ""
-}
-var player = "";
-var cpu = "";
-var counter = 0;
-var human = 0;
-var computer = 0;
+// Game state management
+const gameState = {
+    board: {
+        a1: "", a2: "", a3: "",
+        b1: "", b2: "", b3: "",
+        c1: "", c2: "", c3: ""
+    },
+    player: "",
+    computer: "",
+    counter: 0,
+    scores: {
+        human: 0,
+        computer: 0
+    },
+    isGameActive: false,
+    difficulty: 'medium' // Default difficulty
+};
 
-// functions***************
+// Game constants
+const WINNING_COMBINATIONS = [
+    ['a1', 'a2', 'a3'], ['b1', 'b2', 'b3'], ['c1', 'c2', 'c3'], // Rows
+    ['a1', 'b1', 'c1'], ['a2', 'b2', 'c2'], ['a3', 'b3', 'c3'], // Columns
+    ['a1', 'b2', 'c3'], ['a3', 'b2', 'c1'] // Diagonals
+];
 
-function setComputer(value) {
-  cpu = value;
-}
-
-function getComputer() {
-  return cpu;
-}
-
-function setPlayer(value) {
-  player = value;
-}
-
-function getPlayer() {
-  return player;
-}
-
-function resetBoard() {
-  console.log("board is reset!")
-
-  for (var key in board) {
-    $('#move').remove();
-    board[key] = "";
-  }
-  $("#playO").html("play as O");
-  $("#playX").html("play as X");
-  setPlayer("");
-  setComputer("");
-  counter = 0;
-}
-
-function incScoreBoard(xO) {
-  if (getPlayer() == xO) {
-    human += 1;
-    alert("You Won!")
-  } else {
-    computer += 1;
-    alert("Computer Wins!")
-  }
-  resetBoard();
-  $("#scoreBoard").html("The score is human: " + human + " computer: " + computer)
-
-}
-
-
-
-function checkForWin(inc) {
-  console.log("checkForWin was fired");
-  var pass = true;
-  var checkRows = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
-  counter += inc;
-
-  if (counter < 9) {
-    for (var i = 0; i < 8; i++) {
-      if (board[Object.keys(board)[checkRows[i][0]]] == board[Object.keys(board)[checkRows[i][1]]] && board[Object.keys(board)[checkRows[i][0]]] == board[Object.keys(board)[checkRows[i][2]]]) {
-        if (board[Object.keys(board)[checkRows[i][0]]] !== "") {
-          if (board[Object.keys(board)[checkRows[i][1]]] !== "") {
-            if (board[Object.keys(board)[checkRows[i][2]]] !== "") {
-              incScoreBoard(board[Object.keys(board)[checkRows[i][2]]]);
-              // alert("Winner!");
-              pass = false;
-            }
-          }
-        }
-      }
-    } //forloop ending
-  } //end of top if
-  else {
-    alert("tie game!")
-    resetBoard();
-    pass = false;
-  }
-  return pass;
-} //function ending
-
-function checkForEmpty(value) {
-  var empty = true;
-  if (value != "") {
-    empty = false;
-  }
-  return empty;
-}
-
-function playerPicked(value) {
-  if (getPlayer() === "") {
-
-    if (value === "#playO") {
-
-      $("#playX").html("Computer is X");
-      setComputer("X");
-      setPlayer("O");
-      $("#playO").html("You are O");
-
-    } else if (value === "#playX") {
-
-      $("#playO").html("Computer is O");
-      setComputer("O");
-      setPlayer("X");
-      $("#playX").html("You are X");
-
+// DOM Elements
+const elements = {
+    cells: document.querySelectorAll('.game-cell'),
+    playXBtn: document.getElementById('playX'),
+    playOBtn: document.getElementById('playO'),
+    resetBtn: document.getElementById('reset'),
+    humanScore: document.getElementById('humanScore'),
+    computerScore: document.getElementById('computerScore'),
+    gameStatus: document.getElementById('gameStatus'),
+    difficultyButtons: {
+        easy: document.getElementById('easyMode'),
+        medium: document.getElementById('mediumMode'),
+        hard: document.getElementById('hardMode')
     }
-  } else {
-    alert('press "reset game" to switch your letter');
-  }
-}
+};
 
-function playerPickedLetter() {
-  var pass = true;
-  if (getPlayer() === "") {
-    pass = false;
-  }
-  return pass;
-}
+// Event Listeners
+document.addEventListener('DOMContentLoaded', () => {
+    elements.playXBtn.addEventListener('click', () => selectPlayer('X'));
+    elements.playOBtn.addEventListener('click', () => selectPlayer('O'));
+    elements.resetBtn.addEventListener('click', resetGame);
+    elements.cells.forEach(cell => {
+        cell.addEventListener('click', () => handleCellClick(cell));
+    });
 
-function checkForTaken(input){
-  var pass = false;
-  var key = Object.keys(board)[input];
-  var value = board[key];
-  if(checkForEmpty(value)){
-    pass = true;
-  }else{
-    alert("This sqaure is taken try another square!")
-  }
-  return pass; 
-}
+    // Difficulty selection
+    Object.entries(elements.difficultyButtons).forEach(([difficulty, button]) => {
+        button.addEventListener('click', () => selectDifficulty(difficulty));
+    });
 
-function playersChoice(input, XorO) {
+    // Set default difficulty
+    elements.difficultyButtons.medium.classList.add('active');
+});
 
-  var key = Object.keys(board)[input];
-  
-    board[key] = XorO;
-    $('#' + key).append("<p id='move'>" + XorO + " </p>");
-  
- 
-}
-
-//       winOrBlock
-function winOrBlock(xO) {
-  console.log("winOrBlock was fired!");
-  var checkRows = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
-  var found = false;
-
-  if (!found) {
-    //look for win or block
-    for (var i = 0; i < 8; i++) {
-      if (board[Object.keys(board)[checkRows[i][0]]] == board[Object.keys(board)[checkRows[i][1]]]) {
-        if (board[Object.keys(board)[checkRows[i][0]]] == xO) {
-          if (board[Object.keys(board)[checkRows[i][1]]] == xO) {
-            if (board[Object.keys(board)[checkRows[i][2]]] == "") {
-              playersChoice(checkRows[i][2], getComputer());
-              found = true;
-              break;
-            }
-          }
-        }
-      } //end of top if                                                       
-    } //forloop ending
-  }
-  if (!found) {
-
-    for (var i = 0; i < 8; i++) {
-      if (board[Object.keys(board)[checkRows[i][0]]] == board[Object.keys(board)[checkRows[i][2]]]) {
-        if (board[Object.keys(board)[checkRows[i][0]]] == xO) {
-          if (board[Object.keys(board)[checkRows[i][2]]] == xO) {
-            if (board[Object.keys(board)[checkRows[i][1]]] == "") {
-              playersChoice(checkRows[i][1], getComputer());
-              found = true;
-              break;
-            }
-          }
-        }
-      } //end of top if                                                       
-    } //forloop ending
-  }
-  if (!found) {
-    for (var i = 0; i < 8; i++) {
-      if (board[Object.keys(board)[checkRows[i][1]]] == board[Object.keys(board)[checkRows[i][2]]]) {
-        if (board[Object.keys(board)[checkRows[i][2]]] == xO) {
-          if (board[Object.keys(board)[checkRows[i][1]]] == xO) {
-            if (board[Object.keys(board)[checkRows[i][0]]] == "") {
-              playersChoice(checkRows[i][0], getComputer());
-              found = true;
-              break;
-            }
-          }
-        }
-      } //end of top if                                                       
-    } //forloop ending
-  }
-  return found;
-} //end of function
-
-function setAdvantage() {
-  console.log("setAdvantage was fired!");
-  
-  if(board.a1 == getPlayer() && board.c3 == getPlayer()){
+// Difficulty Selection
+function selectDifficulty(difficulty) {
+    gameState.difficulty = difficulty;
     
-    playersChoice(1, getComputer());
-    
-  } else if (board.c1 == getPlayer() && board.a3 == getPlayer()) {
-    
-    playersChoice(7, getComputer());
-    
-  } else if (checkForEmpty(board.b2)) {
+    // Update button states
+    Object.entries(elements.difficultyButtons).forEach(([diff, button]) => {
+        button.classList.toggle('active', diff === difficulty);
+    });
 
-    playersChoice(4, getComputer());
-
-  } else if (checkForEmpty(board.a3)) {
-
-    playersChoice(2, getComputer());
-
-  } else if (checkForEmpty(board.a1)) {
-
-    playersChoice(0, getComputer());
-
-  } else if (checkForEmpty(board.c1)) {
-
-    playersChoice(6, getComputer());
-
-  } else if (checkForEmpty(board.c2)) {
-
-    playersChoice(7, getComputer());
-
-  } else if (checkForEmpty(board.a2)) {
-
-    playersChoice(1, getComputer());
-
-  } else if (checkForEmpty(board.b1)) {
-
-    playersChoice(3, getComputer())
-
-  } else if (checkForEmpty(board.b3)) {
-
-    playersChoice(5, getComputer());
-
-  } else if (checkForEmpty(board.c1)) {
-
-    playersChoice(6, getComputer());
-
-  } else if (checkforEmpty(board.c3)) {
-
-    playersChoice(8, getComputer());
-
-  }
-
+    updateGameStatus(`Difficulty set to ${difficulty.charAt(0).toUpperCase() + difficulty.slice(1)}`);
 }
 
-function aiMove() {
-  console.log("aiMove was fired!")
-  var move = false;
-  if (!move) {
-    var xO = getComputer();
-    move = winOrBlock(xO);
+// Game Logic Functions
+function selectPlayer(symbol) {
+    if (gameState.isGameActive) {
+        updateGameStatus('Please reset the game to change your symbol');
+        return;
+    }
 
-  }
-  if (!move) {
-    var xO = getPlayer();
-    move = winOrBlock(xO);
-  }
-  if (!move) {
-    setAdvantage();
-  }
+    gameState.player = symbol;
+    gameState.computer = symbol === 'X' ? 'O' : 'X';
+    gameState.isGameActive = true;
+    
+    updateGameStatus(`You are playing as ${symbol}`);
+    updateButtonStates();
+    
+    if (gameState.computer === 'X') {
+        setTimeout(computerMove, 500);
+    }
 }
 
-// actions**************************************
-
-$("#playX").click(function() {
-  playerPicked("#playX")
-});
-
-$("#playO").click(function() {
-  playerPicked("#playO");
-});
-
-// $('#reset').click(function() {
-//   resetBoard();
-// });
-
-$('#a1').click(function() {
-
-  if (playerPickedLetter() && checkForTaken(0)) {   // todo: if(playerPickedLetter() && <<new function here>>()){} --> add a new function that uses the helper function checkForEmpty and remove alert form playerChoice
-    playersChoice(0, getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
+function handleCellClick(cell) {
+    if (!gameState.isGameActive) {
+        updateGameStatus('Please select your symbol first');
+        return;
     }
-  }
 
-});
-
-$('#a2').click(function() {
-
-  if (playerPickedLetter() && checkForTaken(1)) {
-    playersChoice(1, getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
+    const cellId = cell.id;
+    if (gameState.board[cellId] !== "") {
+        updateGameStatus('This cell is already taken!');
+        return;
     }
-  }
 
-});
+    makeMove(cellId, gameState.player);
+    if (checkGameEnd()) return;
 
-$('#a3').click(function() {
+    setTimeout(computerMove, 500);
+}
 
-  if (playerPickedLetter() && checkForTaken(2)) {
-    playersChoice(2,getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
+function computerMove() {
+    if (!gameState.isGameActive) return;
+
+    const move = findBestMove();
+    makeMove(move, gameState.computer);
+    checkGameEnd();
+}
+
+function findBestMove() {
+    const difficultySettings = {
+        easy: {
+            smartMoveChance: 0.3,
+            blockChance: 0.2,
+            centerChance: 0.3,
+            cornerChance: 0.2
+        },
+        medium: {
+            smartMoveChance: 0.7,
+            blockChance: 0.5,
+            centerChance: 0.7,
+            cornerChance: 0.6
+        },
+        hard: {
+            smartMoveChance: 1,
+            blockChance: 1,
+            centerChance: 1,
+            cornerChance: 1
+        }
+    };
+
+    const settings = difficultySettings[gameState.difficulty];
+
+    // Try to win
+    const winningMove = findWinningMove(gameState.computer);
+    if (winningMove) return winningMove;
+
+    // Block player's winning move based on difficulty
+    if (Math.random() < settings.blockChance) {
+        const blockingMove = findWinningMove(gameState.player);
+        if (blockingMove) return blockingMove;
     }
-  }
-});
 
-$('#b1').click(function() {
-  if (playerPickedLetter() && checkForTaken(3)) {
-    playersChoice(3, getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
+    // Take center based on difficulty
+    if (Math.random() < settings.centerChance && gameState.board.b2 === "") {
+        return 'b2';
     }
-  }
-});
 
-$('#b2').click(function() {
-  if (playerPickedLetter() && checkForTaken(4)) {
-    playersChoice(4, getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
+    // Take corners based on difficulty
+    if (Math.random() < settings.cornerChance) {
+        const corners = ['a1', 'a3', 'c1', 'c3'];
+        const availableCorners = corners.filter(corner => gameState.board[corner] === "");
+        if (availableCorners.length > 0) {
+            return availableCorners[Math.floor(Math.random() * availableCorners.length)];
+        }
     }
-  }
-});
 
-$('#b3').click(function() {
-  if (playerPickedLetter() && checkForTaken(5)) {
-    playersChoice(5, getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
-    }
-  }
-});
+    // Take any available space
+    const availableMoves = Object.entries(gameState.board)
+        .filter(([_, value]) => value === "")
+        .map(([key]) => key);
+    return availableMoves[Math.floor(Math.random() * availableMoves.length)];
+}
 
-$('#c1').click(function() {
-  if (playerPickedLetter() && checkForTaken(6)) {
-    playersChoice(6, getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
+function findWinningMove(symbol) {
+    for (const combination of WINNING_COMBINATIONS) {
+        const [a, b, c] = combination;
+        const values = [gameState.board[a], gameState.board[b], gameState.board[c]];
+        
+        if (values.filter(v => v === symbol).length === 2 && 
+            values.filter(v => v === "").length === 1) {
+            return combination[values.indexOf("")];
+        }
     }
-  }
-});
+    return null;
+}
 
-$('#c2').click(function() {
-  if (playerPickedLetter() && checkForTaken(7)) {
-    playersChoice(7, getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
-    }
-  }
-});
+function makeMove(cellId, symbol) {
+    gameState.board[cellId] = symbol;
+    gameState.counter++;
+    
+    const cell = document.getElementById(cellId);
+    cell.textContent = symbol;
+    cell.classList.add(symbol.toLowerCase());
+}
 
-$('#c3').click(function() {
-  if (playerPickedLetter() && checkForTaken(8)) {
-    playersChoice(8, getPlayer());
-    if(checkForWin(1)){
-        aiMove();
-        checkForWin(1);
+function checkGameEnd() {
+    // Check for win
+    for (const combination of WINNING_COMBINATIONS) {
+        const [a, b, c] = combination;
+        if (gameState.board[a] && 
+            gameState.board[a] === gameState.board[b] && 
+            gameState.board[a] === gameState.board[c]) {
+            handleGameEnd(gameState.board[a]);
+            return true;
+        }
     }
-  }
-});
+
+    // Check for tie
+    if (gameState.counter === 9) {
+        handleGameEnd(null);
+        return true;
+    }
+
+    return false;
+}
+
+function handleGameEnd(winner) {
+    gameState.isGameActive = false;
+    
+    if (winner) {
+        if (winner === gameState.player) {
+            gameState.scores.human++;
+            updateGameStatus('Congratulations! You won!');
+        } else {
+            gameState.scores.computer++;
+            updateGameStatus('Game Over! Computer won!');
+        }
+    } else {
+        updateGameStatus('It\'s a tie!');
+    }
+    
+    updateScoreBoard();
+    updateButtonStates();
+}
+
+// UI Update Functions
+function updateGameStatus(message) {
+    elements.gameStatus.textContent = message;
+}
+
+function updateScoreBoard() {
+    elements.humanScore.textContent = gameState.scores.human;
+    elements.computerScore.textContent = gameState.scores.computer;
+}
+
+function updateButtonStates() {
+    elements.playXBtn.disabled = gameState.isGameActive;
+    elements.playOBtn.disabled = gameState.isGameActive;
+    elements.resetBtn.disabled = !gameState.isGameActive && gameState.counter === 0;
+}
+
+function resetGame() {
+    // Reset board
+    Object.keys(gameState.board).forEach(key => {
+        gameState.board[key] = "";
+        const cell = document.getElementById(key);
+        cell.textContent = "";
+        cell.classList.remove('x', 'o');
+    });
+
+    // Reset game state
+    gameState.counter = 0;
+    gameState.isGameActive = false;
+    gameState.player = "";
+    gameState.computer = "";
+
+    // Reset UI
+    updateGameStatus('Choose your symbol to start playing!');
+    updateButtonStates();
+    elements.playXBtn.textContent = 'Play as X';
+    elements.playOBtn.textContent = 'Play as O';
+}
